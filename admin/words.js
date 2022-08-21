64f9e6b9cbd162e6aa64c462b0963ab6c719be55
ioBroker Adapter Creator <noreply@iobroker.net>
2022-08-21 21:59:33 +0000

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,29 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"lighteffects adapter settings": {
+		en: "Adapter settings for lighteffects",
+		de: "Adaptereinstellungen für lighteffects",
+		ru: "Настройки адаптера для lighteffects",
+		pt: "Configurações do adaptador para lighteffects",
+		nl: "Adapterinstellingen voor lighteffects",
+		fr: "Paramètres d'adaptateur pour lighteffects",
+		it: "Impostazioni dell'adattatore per lighteffects",
+		es: "Ajustes del adaptador para lighteffects",
+		pl: "Ustawienia adaptera dla lighteffects",
+		"zh-cn": "lighteffects的适配器设置",
+	},
+	option1: {
+		en: "option1",
+		de: "option1",
+		ru: "option1",
+		pt: "option1",
+		nl: "option1",
+		fr: "option1",
+		it: "option1",
+		es: "option1",
+		pl: "option1",
+		"zh-cn": "option1",
+	},
+};
